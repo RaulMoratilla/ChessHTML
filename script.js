83171b260909019d0d8ec107dd0af1b882b76b71
RaulMoratilla <raul.moratilla@edu.uah.es>
2023-11-28 19:59:51 +0100

    Se mueven

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const white_piece = ['w_rook', 'w_horse', 'w_bishop', 'w_queen', 'w_king', 'w_bishop', 'w_horse', 'w_rook'];
-const black_piece = ['b_rook', 'b_horse', 'b_bishop', 'b_king', 'b_queen', 'b_bishop', 'b_horse', 'b_rook'];
+const black_piece = ['b_rook', 'b_horse', 'b_bishop', 'b_queen', 'b_king', 'b_bishop', 'b_horse', 'b_rook'];
 
+// Variable global para almacenar la pieza actualmente arrastrada
+let draggedPiece = null;
+let originalCell = null;
+
+// Variables globales para almacenar las posiciones iniciales del ratón con respecto a la pieza
+let offsetX, offsetY;
 
 function fillBoard() {
     const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -12,8 +18,6 @@ function fillBoard() {
             cell.classList.add('cell');
             cell.classList.add((fil + col) % 2 == 0 ? 'white-cell' : 'black-cell');
             cell.id = letters[7 - fil] + String(col + 1);
-            cell.ondrop = drop;
-            cell.ondragover = allowDrop;
             board.appendChild(cell);
         }
     }
@@ -36,6 +40,8 @@ function fillBoard() {
 
         const black_big_piece = createPiece(black_piece[col - 1]);
         black_big_piece_cell.appendChild(black_big_piece);
+
+        createListeners();
     }
 }
 
@@ -44,38 +50,145 @@ function createPiece(pieceClass) {
     piece.src = "img/" + pieceClass + ".png";
     piece.alt = pieceClass;
     piece.classList.add('piece_cell');
+    switch (pieceClass.charAt(0)) {
+        case 'w':
+            piece.classList.add('white_piece');
+            break;
+        case 'b':
+            piece.classList.add('black_piece');
+            break;
+    }
     piece.classList.add(pieceClass);
-    piece.draggable = true;
-    piece.ondragstart = drag;
     return piece;
 }
 
-function drag(event) {
-    // Obtén la URL de la imagen desde el src del elemento img
-    const data = event.target.src;
-
-    // Establece el elemento img como el elemento arrastrado
-    event.dataTransfer.setData("text", data);
-
-    event.target.parentNode.removeChild(event.target);
+function createListeners() {
+    document.querySelectorAll('.piece_cell').forEach(piece => {
+        piece.addEventListener('mousedown', startDragging);
+    });
 }
 
-function allowDrop(event) {
-    // Previene el comportamiento predeterminado para permitir soltar
+function startDragging(event) {
+    // Evitar comportamiento predeterminado del evento
     event.preventDefault();
+
+    // Obtén la pieza que se está arrastrando
+    draggedPiece = event.target;
+
+    // Calcula el offset para centrar la pieza en el puntero del ratón
+    offsetX = draggedPiece.offsetWidth / 2;
+    offsetY = draggedPiece.offsetHeight / 2;
+
+    // Ajusta el estilo de la imagen para mantener su tamaño original
+    draggedPiece.style.width = draggedPiece.offsetWidth + 'px';
+    draggedPiece.style.height = draggedPiece.offsetHeight + 'px';
+
+    // Agrega un controlador de eventos de ratón para seguir el movimiento del ratón
+    document.addEventListener('mousemove', updatePosition);
+
+    // Remueve el listener de soltar asociado al documento para evitar problemas
+    document.removeEventListener('mouseup', dropPiece);
+
+    // Agrega el listener de soltar asociado a la pieza
+    draggedPiece.addEventListener('mouseup', dropPiece);
+
+    // Establece la opacidad de la pieza mientras se arrastra
+    draggedPiece.style.opacity = '1';
+
+    // Establece el cursor "grabbing"
+    document.body.style.cursor = 'grabbing';
+
+    // Elimina la pieza de la casilla original
+    originalCell = draggedPiece.parentNode;
+    originalCell.removeChild(draggedPiece);
+
+    // Añade la pieza al cuerpo del documento para que no quede restringida por tamaños de contenedores
+    document.body.appendChild(draggedPiece);
+
+    updatePosition(event);
 }
 
-function drop(event) {
-    // Obtiene la URL de la imagen desde los datos de transferencia
-    const data = event.dataTransfer.getData("text");
 
-    console.log(event.dataTransfer)
-
-    // Crea un nuevo elemento img con la misma URL
-    const draggedElement = event.target;
-
-    console.log(draggedElement);
-
-    // Agrega el elemento img al destino
-    event.target.appendChild(draggedElement);
+function updatePosition(e) {
+    // Actualiza la posición de la pieza mientras se mueve el ratón
+    draggedPiece.style.position = 'absolute';
+    draggedPiece.style.left = e.clientX - offsetX + 'px';
+    draggedPiece.style.top = e.clientY - offsetY + 'px';
 }
+
+function findTargetCell(mouseX, mouseY) {
+    // Itera sobre todas las celdas y verifica si el mouse está dentro de los límites de cada celda
+    const cells = document.querySelectorAll('.cell');
+    for (const cell of cells) {
+        const rect = cell.getBoundingClientRect();
+        if (
+            mouseX >= rect.left &&
+            mouseX <= rect.right &&
+            mouseY >= rect.top &&
+            mouseY <= rect.bottom
+        ) {
+            return cell;
+        }
+    }
+    return null;
+}
+
+function dropPiece(event) {
+    if (draggedPiece) {
+        // Elimina los controladores de eventos de ratón
+        document.removeEventListener('mousemove', updatePosition);
+        draggedPiece.removeEventListener('mouseup', dropPiece);
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        let targetCell = findTargetCell(mouseX, mouseY);
+
+        if (!targetCell || !checkMove(originalCell, targetCell, draggedPiece.alt)) {
+            targetCell = originalCell;
+        }
+
+        // Ajusta el estilo de la imagen para centrarla en la casilla
+        const offsetX = (targetCell.offsetWidth - draggedPiece.offsetWidth) / 2;
+        const offsetY = (targetCell.offsetHeight - draggedPiece.offsetHeight) / 2;
+
+        // Restablece la posición de la pieza a la posición de la casilla más el offset
+        draggedPiece.style.left = targetCell.getBoundingClientRect().left + offsetX + 'px';
+        draggedPiece.style.top = targetCell.getBoundingClientRect().top + offsetY + 'px';
+        targetCell.appendChild(draggedPiece);
+
+        // Restaura el cursor a su valor original
+        document.body.style.cursor = 'auto';
+
+        draggedPiece = null;
+        originalCell = null;
+    }
+}
+
+function checkMove(originalCell, targetCell, piece) {
+    let originalCellId = originalCell.id;
+    let targetCellId = targetCell.id;
+    let originalCellLetter = originalCellId.charAt(0);
+    let originalCellNumber = originalCellId.charAt(1);
+    let targetCellLetter = targetCellId.charAt(0);
+    let targetCellNumber = targetCellId.charAt(1);
+    let move = false;
+    console.log(originalCellLetter, originalCellNumber, targetCellLetter, targetCellNumber);
+    console.log(targetCell.hasChildNodes());
+    console.log(targetCell);
+    if (targetCell.hasChildNodes()) {
+        console.log(targetCell.lastChild.classList.contains('black_piece'));
+    }
+    switch (piece) {
+        case 'w_pawn':
+            if ((originalCellLetter.charCodeAt(0)+1 == targetCellLetter.charCodeAt(0) && targetCellNumber == originalCellNumber && !targetCell.hasChildNodes())
+                || (originalCellLetter.charCodeAt(0)+2 == targetCellLetter.charCodeAt(0) && targetCellNumber == originalCellNumber && originalCellLetter == 'b' && !targetCell.hasChildNodes())
+                || (originalCellLetter.charCodeAt(0)+1 == targetCellLetter.charCodeAt(0) && targetCellNumber+1 == originalCellNumber && targetCell.hasChildNodes() && targetCell.lastChild.classList.contains('black_piece'))
+                || (originalCellLetter.charCodeAt(0)+1 == targetCellLetter.charCodeAt(0) && targetCellNumber-1 == originalCellNumber-1 && targetCell.hasChildNodes() && targetCell.lastChild.classList.contains('black_piece'))) {
+                
+                console.log(move = true);
+            }
+            break;
+        
+    }
+    return move;
+}
+
